3839cb7d60daef84fc3cd5c63c9c62556c7161de
AgileQ <adxmgr@gmail.com>
2023-04-11 17:54:34 -0400

    Added tokens

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,11 +1,3 @@
 const Hello = (name?: string) => `${name || 'World'}`;
 
-const user = {
-  jobTitle: 'Software Engineer',
-};
-
-if ((user.jobTitle = 'Junior Enginner')) {
-  user.jobTitle = 'Bug Introducer';
-}
-
 export default Hello;
